8d829a2c4f61d2376df44a474df598ba4ba37ccf
TolmachevAleksander <78648515+TolmachevAleksander@users.noreply.github.com>
2021-02-06 17:51:52 +0300

    Corrected comments

--- apologybot.php
+++ apologybot.php
@@ -10,9 +10,9 @@ while ($a <= 42) {                                                              
     preg_match_all('/sfst">.+?<\/p>/', $description, $text);                                   // Применяем регулярное выражение.
     $AllApologys = $text[0];                                                                   // Получаем массив с извинениями.
     foreach ($AllApologys as $arr){                                                            // Запускаем цикл с перебором всех извинений.
-	    preg_match_all('/[^sfstbrp><\/"&anclquo=;]+/', $arr, $text2);                          // Регулярным выражением удаляем ненужные символы.
-	    $apology = implode("\n", $text2[0]);                                                   // Преобразуем элементы массива в строку.
-	    exec('/usr/bin/php /.../bot/bot.php "'.$apology.'"');                      			   // Отправляем извинение с сайта (Linux).
+	    preg_match_all('/[^sfstbrp><\/"&anclquo=;]+/', $arr, $text2);                      // Регулярным выражением удаляем ненужные символы.
+	    $apology = implode("\n", $text2[0]);                                               // Преобразуем элементы массива в строку.
+	    exec('/usr/bin/php /.../bot/bot.php "'.$apology.'"');                      	       // Отправляем извинение с сайта (Linux).
         sleep(900);                                                                            // Останавливаем скрипт на 15 мин.
     }
     $a++;                                                                                      // Переходим на следующую страницу.
